30af120744768e7e16e0e7cb6a50506278bf02c1
landowsr <landowsr@oregonstate.edu>
2024-02-07 14:19:02 -0800

    test db connection

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function testDB() {
+  try {
+    const res = await pool.query("SELECT NOW()");
+    console.log(
+      "Connection successful, current time from DB:",
+      res.rows[0].now
+    );
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+}
+
+testDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
